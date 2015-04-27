ef66bf140c7aaa8764a459ad4ec3565fb326c3e5
Adriaan <adriaan.rol@gmail.com>
2015-04-27 15:54:48 +0200

    Code for going up to level 5
    Added stair avoidance so that complete level is cleared before moving on

--- pythonwarrior/ram-intermediate/player.py
+++ pythonwarrior/ram-intermediate/player.py
@@ -10,8 +10,9 @@ class Player(object):
 
         Possible list of improvements to get more points
         a. Run for exit without healing if possible to get time bonus
-        b. clear a level before going to the stairs
-        c. Free prisoners for bonus points
+        b. clear a level before going to the stairs (stair avoidance)
+                (could be improved by not using random direction)
+        c. Clearing of all mobs
         '''
 
         self.warrior = warrior
@@ -19,9 +20,6 @@ class Player(object):
 
         # Move if current surroundings are clear
         self.sounds = warrior.listen()
-        print 'Ram hears sounds the sounds coming from: %s' % self.sounds
-
-
 
         if self.check_rest_needed() & self.check_safe_to_rest():
             warrior.rest_()
@@ -38,15 +36,42 @@ class Player(object):
             self.free_captive()
             return
         else:
-            direction = self.determine_direction()
+            direction = self.determine_direction_of_next_objective()
+            if self.sounds != []:
+                direction = self.avoid_stairs(direction)
             warrior.walk_(direction)
             return
 
-    def determine_direction(self):
+    def get_possible_directions(self):
+        possible_directions = ['forward', 'backward', 'left', 'right']
+        for direction, place in self.surroundings.iteritems():
+            if place.is_wall():
+                possible_directions.remove(direction)
+        return possible_directions
+
+    def avoid_stairs(self, direction):
+        possible_directions = self.get_possible_directions()
+        if self.warrior.feel(direction).is_stairs():
+            print " Ram don't want to go up stairs yet, changing direction'"
+            possible_directions.remove(direction)
+            direction = possible_directions[0]
+
+        return direction
+
+
+    def determine_direction_of_next_objective(self):
+        '''
+        Goes over all sounds and sets direction to run towards first sound.
+        No smartness in here yet.
+        '''
         for sound in self.sounds:
             if sound.is_captive():
                 direction = self.warrior.direction_of(sound)
                 return direction
+            elif sound.is_enemy():
+                direction = self.warrior.direction_of(sound)
+                return direction
+
         direction = self.warrior.direction_of_stairs()
         return direction
 
@@ -100,6 +125,9 @@ class Player(object):
 
     def check_rest_needed(self):
         health = self.warrior.health()
+        if self.sounds is None:
+            print 'Ram no need rest! all work is done. [%s HP]' % health
+            return False
         if health < 19:
             print 'Ram is feeling weak, he needs to rest... [%s HP]' % health
             return True
