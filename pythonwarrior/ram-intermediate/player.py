73f23d2a4da0e343865e221487beb6569d5050d2
Adriaan <adriaan.rol@gmail.com>
2015-04-27 15:20:23 +0200

    First pass of level 4

--- pythonwarrior/ram-intermediate/player.py
+++ pythonwarrior/ram-intermediate/player.py
@@ -5,31 +5,39 @@ class Player(object):
         1. Get information on surroundings
         2. Take care of personal safety.
         3. Move towards objective.
+           1. Free all prisoners
+           2. Move to stairs
 
         Possible list of improvements to get more points
         a. Run for exit without healing if possible to get time bonus
         b. clear a level before going to the stairs
+        c. Free prisoners for bonus points
         '''
 
         self.warrior = warrior
-        direction_of_stairs = warrior.direction_of_stairs()
         self.surroundings = self.sense_surroundings()
-        if self.check_rest_needed() & self.check_surroundings_safe():
+
+        # Move if current surroundings are clear
+        sounds = warrior.listen()
+
+        print 'Ram hears sounds in the following directions %s' % sounds
+
+        if self.check_rest_needed() & self.check_safe_to_rest():
             warrior.rest_()
             return
         elif self.check_threatened():
             print 'Ram is feeling threatened, starting to bind enemies'
             self.bind_adjacent_enemy()
             return
-
-        if self.surroundings[direction_of_stairs].is_enemy():
-            print 'Attacking %s in %s direction' % (
-                self.surroundings[direction_of_stairs],
-                direction_of_stairs)
-            warrior.attack_(direction_of_stairs)
+        elif self.check_No_Enemies_near() >= 1:
+            print 'Ram is confident he can slay this beast! Attacking!'
+            self.attack_adjacent_enemy()
+            return
+        elif self.check_captive_near():
+            self.free_captive()
             return
         else:
-            warrior.walk_(direction_of_stairs)
+            warrior.walk_(warrior.direction_of_stairs())
             return
 
     def bind_adjacent_enemy(self):
@@ -38,21 +46,44 @@ class Player(object):
                 self.warrior.bind_(direction)
                 return
 
-    def check_surroundings_safe(self):
+    def attack_adjacent_enemy(self):
+        for direction, place in self.surroundings.iteritems():
+            if place.is_enemy():
+                self.warrior.attack_(direction)
+                return
+
+    def free_captive(self):
+        for direction, place in self.surroundings.iteritems():
+            if place.is_captive():
+                self.warrior.rescue_(direction)
+                return
+
+    def check_safe_to_rest(self):
         safe = True
         for place in self.surroundings.itervalues():
             if place.is_enemy():
                 safe = False
         return safe
 
-    def check_threatened(self):
-        nOofEnemies = 0
+    def check_No_Enemies_near(self):
+        NoOfEnemies = 0
         for place in self.surroundings.itervalues():
             if place.is_enemy():
-                nOofEnemies += 1
-        if nOofEnemies >= 2:
+                NoOfEnemies += 1
+        return NoOfEnemies
+
+    def check_captive_near(self):
+        NoOfPrisoners = 0
+        for place in self.surroundings.itervalues():
+            if place.is_captive():
+                NoOfPrisoners += 1
+        return NoOfPrisoners
+
+    def check_threatened(self):
+        NoOfEnemies = self.check_No_Enemies_near()
+        if NoOfEnemies >= 2:
             return True
-        elif nOofEnemies >= 1 and self.warrior.health() < 5:
+        elif NoOfEnemies >= 1 and self.warrior.health() < 5:
             return True
         else:
             return False
